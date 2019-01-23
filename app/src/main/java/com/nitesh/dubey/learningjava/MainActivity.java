b5d10460efed46c0ac9bb811739609e8ca93cf2e
Nitesh Dubey <niteshdubey100@gmail.com>
2019-01-23 17:32:51 +0530

    Learning Java

---
+++ app/src/main/java/com/nitesh/dubey/learningjava/MainActivity.java
@@ -0,0 +1,41 @@
+package com.nitesh.dubey.learningjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText edttxt1;
+    private TextView txt1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edttxt1 = findViewById(R.id.edttxt1);
+        txt1 = findViewById(R.id.txt1);
+
+        }
+
+        public void btnClick(View Textview){
+
+         int x = Integer.valueOf(edttxt1.getText().toString());
+         String s = "";
+         for(int i = 0;i<15;i++){
+
+             s = s + x + " * " + (i+1) + " = " + (x*(i+1) +"") + "\n";
+
+         }
+
+         txt1.setText(s);
+
+
+        }
+
+
+}
